ef00b8209374c8ee65a0fd1447e6acfb2a450a8d
Dev Raghuwanshi <devraghuwanshi18@gmail.com>
2026-03-16 02:11:23 +0530

    Add REACT Props
    Practice examples demonstrating how React props are used to pass data between components and create reusable UI elements.

---
+++ REACT Props/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Greeting, Details, Default, Card } from "./Props";
+import { UserCard } from "./userCard";
+
+// Parent Component.
+function App() {
+  return (
+    <>
+      <Greeting name="Dev" />
+      <Details name="Dev" age={21} />
+      <Default></Default> {/*Default props */}
+      <Default text="Submit" color="Blue" />
+      <Card>This content is passed as children prop</Card>
+
+      {/* Reusable container */}
+      <UserCard name="Dev" age={21} email="devraghuwanshi@gmail.com" />
+      <UserCard name= "Raj" age ={20} email= "rag@gmail.com" />
+    </>
+  );
+}
+
+export default App;
